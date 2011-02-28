8b7118aef097527dae3a05faa728698495cb3afa
Adam Kerr <ajrkerr@gmail.com>
2011-02-28 13:31:21 -0500

    Fixed a bug

--- lib/active_directory.rb
+++ lib/active_directory.rb
@@ -33,6 +33,9 @@ require 'active_directory/field_type/binary.rb'
 require 'active_directory/field_type/date.rb'
 require 'active_directory/field_type/timestamp.rb'
 require 'active_directory/field_type/dn_array.rb'
+require 'active_directory/field_type/user_dn_array.rb'
+require 'active_directory/field_type/group_dn_array.rb'
+require 'active_directory/field_type/member_dn_array.rb'
 
 module ActiveDirectory
   
